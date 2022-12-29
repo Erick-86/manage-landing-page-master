9db59a6f06e5d1e7a61bb4a55f457cf4504bd204
FreeMan <zelonaire@gmail.com>
2022-12-29 12:35:09 +0000

    mobile body bg class

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mobileOpenMenuBtn = document.getElementById("mobile-open-menu_btn");
 const mobileCloseMenuBtn = document.getElementById("mobile-close-menu_btn");
 const headerNav = document.getElementById("header-nav");
+const mobileBg = document.body
 
 //Showing Mobile Menu When the Open Menu Btn is clicked
 mobileOpenMenuBtn.addEventListener('click', openMenu)
@@ -8,6 +9,7 @@ function openMenu(){
     headerNav.style.display = 'block';
     mobileOpenMenuBtn.style.display = 'none';
     mobileCloseMenuBtn.style.display = 'block'
+    mobileBg.classList.add('mobile-bg')
 }
 
 //Closing Mobile Menu when the close btn is clicked
@@ -15,5 +17,6 @@ mobileCloseMenuBtn.addEventListener('click', closMenu);
 function closMenu(){
     headerNav.style.display = 'none';
     mobileOpenMenuBtn.style.display = 'block';
-    mobileCloseMenuBtn.style.display = 'none'
+    mobileCloseMenuBtn.style.display = 'none';
+    mobileBg.classList.remove('mobile-bg')
 }
