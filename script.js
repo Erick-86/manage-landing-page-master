bc9f258c0b5aa41d91873ae6a8a798930962e5c7
FreeMan <zelonaire@gmail.com>
2022-12-29 11:18:32 +0000

    getting access to the header elements

---
+++ script.js
@@ -0,0 +1,3 @@
+const mobileOpenMenuBtn = document.getElementById("mobile-open-menu_btn");
+const mobileCloseMenuBtn = document.getElementById("mobile-close-menu_btn");
+const headerNav = document.getElementById("header-nav");
